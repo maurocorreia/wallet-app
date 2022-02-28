4b21e035ac0e34efec3c7e44a86abefe69bc493b
Mauro Correia <mauroac1@outlook.com>
2022-02-28 11:09:03 -0300

    MADE IN HEAVEN.

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -17,15 +17,15 @@ class Forms extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.fetchCoins();
+  }
+
   fetchCoins = async () => {
     const url = 'https://economia.awesomeapi.com.br/json/all';
     const api = await fetch(url);
     const apiJSON = await api.json();
     this.setState({ exchangeRates: apiJSON });
-  }
-
-  componentDidMount() {
-    this.fetchCoins();
   }
 
   handleChange = ({ target }) => {
