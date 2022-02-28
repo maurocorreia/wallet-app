6c2724ddf7ab443e21bb152fddc87a67c6458f13
Mauro Correia <mauroac1@outlook.com>
2022-02-28 17:01:05 -0300

    7 almost 8.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 // ACTION TYPES.
 export const LOGIN_DATA = 'LOGIN_DATA';
 export const WALLET_DATA = 'WALLET_DATA';
+export const DELETE_DATA = 'DELETE_DATA';
 
 export const loginData = (email) => ({
   type: LOGIN_DATA,
@@ -10,4 +11,9 @@ export const loginData = (email) => ({
 export const walletData = (data) => ({
   type: WALLET_DATA,
   expenses: data,
+});
+
+export const deleteData = (expenseUnit) => ({
+  type: DELETE_DATA,
+  expenseUnit,
 });
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,4 @@
-import { WALLET_DATA } from '../actions';
+import { DELETE_DATA, WALLET_DATA } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
@@ -9,6 +9,11 @@ export const walletReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case WALLET_DATA:
     return { ...state, expenses: [...state.expenses, action.expenses] };
+  case DELETE_DATA:
+    return {
+      ...state,
+      expenses: state.expenses.filter((expense) => expense !== action.expenseUnit),
+    };
   default:
     return state;
   }
